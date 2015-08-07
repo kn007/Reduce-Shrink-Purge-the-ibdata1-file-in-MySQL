9568115af1d0069888dc76b7ced2a77f708b8a8b
kn007 <kn007@126.com>
2015-08-08 00:15:57 +0800

    init

---
+++ mysql_dump_all_db.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+MYSQL_USER=root
+MYSQL_PASS=rootpassword
+MYSQL_CONN="-u${MYSQL_USER} -p${MYSQL_PASS}"
+
+#
+# Collect all database names except for
+# mysql, information_schema, and performance_schema
+#
+
+SQL="SELECT schema_name FROM information_schema.schemata WHERE schema_name NOT IN"
+SQL="${SQL} ('mysql','information_schema','performance_schema')"
+
+DBLISTFILE=/tmp/DatabasesToDump.txt
+mysql ${MYSQL_CONN} -ANe"${SQL}" > ${DBLISTFILE}
+
+DBLIST=""
+for DB in `cat ${DBLISTFILE}` ; do DBLIST="${DBLIST} ${DB}" ; done
+
+MYSQLDUMP_OPTIONS="--routines --triggers --single-transaction"
+mysqldump ${MYSQL_CONN} ${MYSQLDUMP_OPTIONS} --databases ${DBLIST} > all-dbs.sql
+
+exit
