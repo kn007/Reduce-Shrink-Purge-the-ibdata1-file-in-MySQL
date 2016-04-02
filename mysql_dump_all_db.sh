2623cc39b0ba19c6cfae9baf84e874adfef181bb
Karl Chen <kn007@users.noreply.github.com>
2016-04-02 14:14:07 +0800

    Update mysql_dump_all_db.sh

--- mysql_dump_all_db.sh
+++ mysql_dump_all_db.sh
@@ -6,11 +6,11 @@ MYSQL_CONN="-u${MYSQL_USER} -p${MYSQL_PASS}"
 
 #
 # Collect all database names except for
-# mysql, information_schema, and performance_schema
+# mysql, information_schema, performance_schema and sys
 #
 
 SQL="SELECT schema_name FROM information_schema.schemata WHERE schema_name NOT IN"
-SQL="${SQL} ('mysql','information_schema','performance_schema')"
+SQL="${SQL} ('mysql','information_schema','performance_schema','sys')"
 
 DBLISTFILE=/tmp/DatabasesToDump.txt
 mysql ${MYSQL_CONN} -ANe"${SQL}" > ${DBLISTFILE}
